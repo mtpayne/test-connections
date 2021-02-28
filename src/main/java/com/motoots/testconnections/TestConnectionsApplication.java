c99427d3ab78f2387e3cf2a5d4099b919d1ef73e
Maurice Payne <mauricetpayne@gmail.com>
2021-02-28 14:32:20 -0800

    Initial commit

---
+++ src/main/java/com/motoots/testconnections/TestConnectionsApplication.java
@@ -0,0 +1,13 @@
+package com.motoots.testconnections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConnectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestConnectionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motoots/testconnections/TestConnectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motoots.testconnections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestConnectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
